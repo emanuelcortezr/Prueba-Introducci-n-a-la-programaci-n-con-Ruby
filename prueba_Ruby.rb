d4ca7787f83edf4e4982a695864d20efa431a710
Manuel Cortez <emanuelcorteszr@gmail.com>
2020-01-14 12:53:34 -0300

    Creado metodo photo_count

--- prueba_Ruby.rb
+++ prueba_Ruby.rb
@@ -4,7 +4,7 @@ require 'json'
 api_key = '0Ws2ZgYUf25tTFiHmuyUPhqIvNlRNSTB3ODYu4Eo'
 url_api_key = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=' + api_key
 
-
+# Consumo de una API Rest con autenticaciÃ³n sencilla
 response = RestClient.get(url_api_key)
 
 def build_web_page(response_hash)
@@ -22,3 +22,29 @@ def build_web_page(response_hash)
         f.write("</html>\n")
     end
 end
+
+def count_photos(response_hash)
+    ruby_hash = JSON.parse(response_hash.to_str)
+
+    hash_camera = { 
+        "FHAZ" => 0, 
+        "RHAZ" => 0, 
+        "MAST" => 0,
+        "CHEMCAM" => 0,		
+        "MAHLI"	=> 0,
+        "MARDI"	=> 0,
+        "NAVCAM" => 0,
+        "PANCAM" => 0,
+        "MINITES"=> 0,
+    }
+
+    ruby_hash['photos'].each do |i|
+        camera_name = i['camera']['name'] # "FHAZ"
+        hash_camera[camera_name] = hash_camera[camera_name] + 1
+    end
+
+    return hash_camera
+end
+
+build_web_page(response)
+puts count_photos(response)
